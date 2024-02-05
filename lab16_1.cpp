53b14745d1432f004f32b60d826e0d648e148965
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2024-02-05 23:08:59 +0700

    lab16_1 complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,55 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr,int N,int M){
+	cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << dPtr[i];
+        if(((i+1) % M) == 0) cout << endl;
+        else cout << " ";
+	}
+}
+
+void randData(double *dPtr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        dPtr[i] = (rand() % 101)/ 100.00; 
+    }
+}
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+	int rs1 = 0;
+	double sum = 0;
+	double s[M*N] = {};
+	for(int i = 0; i < N*M; i++){  
+	    sum = sum + dPtr[i];
+	    s[i] = sum;
+	}
+	for(int i = 0; i < N*M; i++){
+        if(M == 1){ sum1[i] = dPtr[i]; rs1++; }
+        if(((i+1) % M) == 0 && i < M && M != 1){ sum1[rs1] = s[i]; rs1++;}
+        if(((i+1) % M) == 0 && i > M){ sum1[rs1] = s[i] - s[(i-M)]; rs1++;}
+	}
+}
+
+
+void findColSum(const double *dPtr,double *sum2,int N,int M){
+	int cs1 = 0;
+	double s[M] = {};
+	
+	for(int cch = 0 ; cch < M ; cch++){
+	    double sum = 0;
+		for(int i = 0; i < N*M; i++){  
+			if((i+1) % M == cch){
+			sum = sum + dPtr[i];
+		    }
+		}
+    if(cch == 0){s[(cch+M)-1] = sum;}
+    s[cch-1] = sum;
+	}
+	
+	for(int i = 0 ; i < M ; i++){
+	    sum2[cs1] = s[i];
+	    cs1++;
+	}
+}
